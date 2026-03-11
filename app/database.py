61684a2a40e051dbe309f63222abd8f1b7ffe83f
Rishi Sai <your@email.com>
2026-03-11 11:04:21 +0530

    added database_url instead of 5 different env variables

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-SQLALCHEMY_DATABASE_URL=f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+import os
+SQLALCHEMY_DATABASE_URL=os.getenv("DATABASE_URL")
 
 engine=create_engine(SQLALCHEMY_DATABASE_URL) 
 
